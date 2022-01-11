beb6146d72e46c44acb34e32b510a11265164cdc
Leanne <leanne_porter@hotmail.com>
2022-01-11 16:53:14 +1100

    updating code

--- recipe_app/src/app/recipes/recipe-list/recipe-list.component.ts
+++ recipe_app/src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Recipe } from "../recipe.model"
 
 @Component({
   selector: 'app-recipe-list',
@@ -6,6 +7,9 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
+  recipes: Recipe[] = [
+    new Recipe('A Test Recipe', 'This is simply a test', 'https://cdn77-s3.lazycatkitchen.com/wp-content/uploads/2021/10/roasted-tomato-sauce-portion-800x1200.jpg')];
+  
 
   constructor() { }
 
